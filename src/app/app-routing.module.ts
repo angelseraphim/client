e69f14565eaab792d19b5328b3e8358eac4e0c00
Angel seraphim <server@praniksl.ru>
2024-12-13 05:41:48 +0500

    test

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -14,6 +14,7 @@ import { AllEventsMapComponent } from './layout/all-events-map/all-events-map.co
 const routes: Routes = [
   {path: 'landpage', component: LandingPageComponent},
   {path: 'login', component: LoginComponent},
+  
   {path: 'register', component: RegisterComponent},
   {path: 'main', component: IndexComponent, canActivate: [AuthGuardService]},
   {
